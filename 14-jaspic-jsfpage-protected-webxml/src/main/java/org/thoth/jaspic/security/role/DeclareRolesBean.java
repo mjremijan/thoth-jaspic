382e7d2f0eff6fba33947b02e8a0b6cb95eadda6
Michael Remijan <mjremijan@yahoo.com>
2017-08-11 12:59:17 -0500

    I got all of the information (session, principal, role) displayed on the page now.  Next is to do the testing.

--- 14-jaspic-jsfpage-protected-webxml/src/main/java/org/thoth/jaspic/security/role/DeclareRolesBean.java
+++
@@ -1,8 +0,0 @@
-package org.thoth.jaspic.security.role;
-
-import javax.annotation.security.DeclareRoles;
-
-@DeclareRoles({"ExecutiveRole", "CustomerSupportRole", "SalaryEmployeeRole"})
-public class DeclareRolesBean {
-
-}
