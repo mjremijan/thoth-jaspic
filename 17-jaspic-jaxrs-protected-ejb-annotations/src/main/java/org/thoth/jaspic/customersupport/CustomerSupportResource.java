6db95235b95cacf331a64ff95102e50229d8eb56
Michael Remijan <mjremijan@yahoo.com>
2017-09-29 15:30:26 -0500

    formatting to make nice screenshot

--- 17-jaspic-jaxrs-protected-ejb-annotations/src/main/java/org/thoth/jaspic/customersupport/CustomerSupportResource.java
+++ 17-jaspic-jaxrs-protected-ejb-annotations/src/main/java/org/thoth/jaspic/customersupport/CustomerSupportResource.java
@@ -5,20 +5,11 @@ import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 
 @Path("customersupport")
 @Stateless
 public class CustomerSupportResource {
-
-    @Context
-    private UriInfo context;
-
-    public CustomerSupportResource() {
-    }
-
     @GET
     @Path("help")
     @Produces(MediaType.APPLICATION_JSON)
