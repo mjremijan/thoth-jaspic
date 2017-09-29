54c6af6a6fae04188cf3345d10ab5cd00ffb5375
Michael Remijan <mjremijan@yahoo.com>
2017-09-29 15:11:49 -0500

    formatting to make nice screenshot

--- 17-jaspic-jaxrs-protected-webxml/src/main/java/org/thoth/jaspic/customersupport/CustomerSupportResource.java
+++ 17-jaspic-jaxrs-protected-webxml/src/main/java/org/thoth/jaspic/customersupport/CustomerSupportResource.java
@@ -3,18 +3,10 @@ package org.thoth.jaspic.customersupport;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 
 @Path("customersupport")
 public class CustomerSupportResource {
-
-    @Context
-    private UriInfo context;
-
-    public CustomerSupportResource() {
-    }
 
     @GET
     @Path("help")
