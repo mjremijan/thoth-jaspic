499bd2730d65c9392789c41a085b82dac92f4884
Michael Remijan <mjremijan@yahoo.com>
2017-10-02 11:23:03 -0500

    formatting to make nice screenshot

--- 17-jaspic-jaxrs-protected-jersey-annotations/src/main/java/org/thoth/jaspic/customersupport/CustomerSupportResource.java
+++ 17-jaspic-jaxrs-protected-jersey-annotations/src/main/java/org/thoth/jaspic/customersupport/CustomerSupportResource.java
@@ -4,19 +4,10 @@ import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 
 @Path("customersupport")
 public class CustomerSupportResource {
-
-    @Context
-    private UriInfo context;
-
-    public CustomerSupportResource() {
-    }
-
     @GET
     @Path("help")
     @Produces(MediaType.APPLICATION_JSON)
